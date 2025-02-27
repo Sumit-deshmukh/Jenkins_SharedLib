b7b61806b6199351873f58df17957ce40afc928e
Sumit deshmukh <114066570+Sumit-deshmukh@users.noreply.github.com>
2025-02-27 20:34:52 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh " docker buildx build --platform linux/amd64 -t ${DockerHubUser}/${ProjectName}:${ImageTag} --load ."
+   sh "sudo docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
