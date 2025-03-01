a59e16c9ba925824d6e4afc82c1d13bc1a68ea05
Sumit deshmukh <114066570+Sumit-deshmukh@users.noreply.github.com>
2025-03-01 13:08:18 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser, String sudoPassword){
-   " echo ${sudoPassword} | sudo -S docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
+/ Define function
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
